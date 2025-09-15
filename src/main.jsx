8896abb9ce6425ca8c4a5cbe215bd63fc5df023a
Adriano Miguel Machado Abreu <adrianexptbr@gmail.com>
2025-09-15 16:32:26 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-
-// CSS global
 import './index.css'
 
 const basename = import.meta.env.VITE_BASE_PATH || '/'
-const rootEl = document.getElementById('root')
-if (!rootEl) throw new Error('#root não encontrado')
 
-ReactDOM.createRoot(rootEl).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename={basename}>
       <App />

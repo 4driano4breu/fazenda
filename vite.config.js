82df9c6f4325ade13c7b936865783977c668d4fb
Adriano Miguel Machado Abreu <adrianexptbr@gmail.com>
2025-09-23 13:51:41 -0400

    Teste

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwind from '@tailwindcss/vite'
-import path from 'node:path'
 
-export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')       // lê VITE_BASE_PATH
-  return defineConfig({
-    plugins: [react(), tailwind()],
-    base: env.VITE_BASE_PATH || '/',                 // << /fazenda/ em produção
-    resolve: {
-      alias: { '@': path.resolve(process.cwd(), 'src') }
-    },
-    build: { outDir: 'dist', sourcemap: true }
-  })
-}
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // subdomínio não precisa de subpasta
+})

ae50b567ef048f170ffe1304e57b6fd0e64c1a97
Adriano Miguel Machado Abreu <adrianexptbr@gmail.com>
2026-02-05 11:23:20 -0400

    Update Supabase

--- src/services/supabaseConfig.js
+++ src/services/supabaseConfig.js
@@ -1,8 +1,8 @@
 // Configuração do Supabase
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://cadxmaouujdvkypulxr.supabase.co';
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhZHhtYW91dWpkdmt5cHVseHIiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTcyNzE5NzQ5NCwiZXhwIjoyMDQyNzczNDk0fQ.VY8ts_Ub5QjKJHKOJgKJQJKJQJKJQJKJQJKJQJKJQJK';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://bkqljchwxosumvfaqoaz.supabase.co';
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrcWxqY2h3eG9zdW12ZmFxb2F6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwMzY2MzUsImV4cCI6MjA4NTYxMjYzNX0.-I1Hhdfikk5SfHONNhjixmE7RbLiC9QTDLdE-tqTz1Q';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
